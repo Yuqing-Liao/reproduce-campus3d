3c9bcb6b6c6a6ef45b6fce0a6c2f8e05cc5cdf45
Yuqing Liao <67686666+Yuqing-Liao@users.noreply.github.com>
2021-02-04 20:03:52 +0800

    Add files via upload

---
+++ utils/io.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author: Yue Wang
+@Contact: yuewangx@mit.edu
+@File: util
+@Time: 4/5/19 3:47 PM
+"""
+class IOStream():
+    def __init__(self, path):
+        self.f = open(path, 'a')
+
+    def cprint(self, text):
+        print(text)
+        self.f.write(text+'\n')
+        self.f.flush()
+
+    def close(self):
+        self.f.close()
