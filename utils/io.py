2f669156656794d38de1aa6907fb9b92dbb43dfa
Yuqing Liao <67686666+Yuqing-Liao@users.noreply.github.com>
2021-02-18 21:03:54 +0800

    Update io.py

--- utils/io.py
+++ utils/io.py
@@ -1,11 +1,6 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-@Author: Yue Wang
-@Contact: yuewangx@mit.edu
-@File: util
-@Time: 4/5/19 3:47 PM
-"""
+import torch
+import torch.nn as nn
+
 class IOStream():
     def __init__(self, path):
         self.f = open(path, 'a')
@@ -17,3 +12,17 @@ class IOStream():
 
     def close(self):
         self.f.close()
+        
+
+def save_model(model, cfg, args, name):
+    if len(cfg.DEVICES.GPU_ID) == 1:
+        torch.save(model.state_dict(), 'checkpoints/%s/models/'+name+'.t7' % (args.exp_name))
+    else:
+        torch.save(model.module.state_dict(), 'checkpoints/%s/models/'+name+'.t7' % (args.exp_name))
+        
+        
+def load_model(args, cfg, model):
+      model.load_state_dict(torch.load(cfg.TRAIN.PRETRAINED_MODEL_PATH))
+      if len(cfg.DEVICES.GPU_ID) > 1:
+          model = nn.DataParallel(model, device_ids=cfg.DEVICES.GPU_ID)
+      return model
