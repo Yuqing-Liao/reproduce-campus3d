8464e22b48c5622f39b528ff79b618f024540953
shinke-li <48745640+shinke-li@users.noreply.github.com>
2021-06-07 23:37:08 +0800

    ACM MM reproducibility review

--- utils/io.py
+++ utils/io.py
@@ -22,7 +22,12 @@ def save_model(model, cfg, args, name):
         
         
 def load_model(args, cfg, model, level=0):
-      model.load_state_dict(torch.load(cfg.TRAIN.PRETRAINED_MODEL_PATH[level]))
-      if len(cfg.DEVICES.GPU_ID) > 1:
-          model = nn.DataParallel(model, device_ids=cfg.DEVICES.GPU_ID)
-      return model
+    model_path = cfg.TRAIN.PRETRAINED_MODEL_PATH
+    if model_path[-1] == ']': #MC models
+        model_path = model_path[1:-1].split(',')
+        model_path = ''.join([s for s in model_path[level] if s not in [' ', '\'', '\"'])
+    assert os.path.isfile(model_path), '\'{}\' model file does not exist.'.format(model_path)
+    model.load_state_dict(torch.load(model_path, map_location=lambda storage, loc: storage))
+    if len(cfg.DEVICES.GPU_ID) > 1:
+        model = nn.DataParallel(model, device_ids=cfg.DEVICES.GPU_ID)
+    return model
